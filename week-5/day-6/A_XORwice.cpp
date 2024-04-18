b61b361ba1cf3a7beafa7b735d61e15c4979f772
samssjubair <samssjubair@gmail.com>
2024-04-18 19:11:44 +0600

    w5 d6

---
+++ week-5/day-6/A_XORwice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+#include <vector>
+#include <iomanip>
+#define ll long long int
+
+using namespace std;
+int main()
+{
+    int t;cin>>t;
+    while(t--){
+        ll a,b;
+        cin>>a>>b;
+    }
+    return 0;
+}
